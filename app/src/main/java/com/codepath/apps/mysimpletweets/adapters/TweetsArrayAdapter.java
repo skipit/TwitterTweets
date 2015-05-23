84f90c24852b379becbc1d0b606640d5783c45c3
skipit <anonymous@anonymous.com>
2015-05-23 11:13:58 -0700

    Abbreviated Relative Time Displayed

--- app/src/main/java/com/codepath/apps/mysimpletweets/adapters/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/adapters/TweetsArrayAdapter.java
@@ -16,6 +16,8 @@ import com.squareup.picasso.Picasso;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -80,14 +82,50 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet> {
 
         String relativeDate = "";
         try {
-            long dateMillis = sf.parse(rawJsonDate).getTime();
-//            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis).toString();
-            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_ABBREV_ALL).toString();
+            Date postedDate = sf.parse(rawJsonDate);
+            relativeDate = getDateDifferenceForDisplay(postedDate);
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
         return relativeDate;
+    }
+
+    //Got from StackOverflow
+    public static String getDateDifferenceForDisplay(Date inputdate) {
+        Calendar now = Calendar.getInstance();
+        Calendar then = Calendar.getInstance();
+
+        now.setTime(new Date());
+        then.setTime(inputdate);
+
+        // Get the represented date in milliseconds
+        long nowMs = now.getTimeInMillis();
+        long thenMs = then.getTimeInMillis();
+
+        // Calculate difference in milliseconds
+        long diff = nowMs - thenMs;
+
+        // Calculate difference in seconds
+        long diffMinutes = diff / (60 * 1000);
+        long diffHours = diff / (60 * 60 * 1000);
+        long diffDays = diff / (24 * 60 * 60 * 1000);
+
+        if (diffMinutes < 60) {
+            return diffMinutes + "m ";
+
+        } else if (diffHours < 24) {
+            return diffHours + "h ";
+
+        } else if (diffDays < 7) {
+            return diffDays + "d ";
+
+        } else {
+
+            SimpleDateFormat todate = new SimpleDateFormat("MMM dd",
+                    Locale.ENGLISH);
+
+            return todate.format(inputdate);
+        }
     }
 }
